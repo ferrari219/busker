d610a98e16355da719dd0808310831a2c8605251
ferrari219@nate.com <ferrari219@nate.com>
2021-10-02 09:50:35 +0900

    스토리북 설치 및 설정 변경

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
+const Dotenv = require("dotenv-webpack");
 module.exports = {
   reactStrictMode: true,
-}
+  babel: {
+    presets: ["@emotion/babel-preset-css-prop"],
+    plugins: ["@emotion"],
+  },
+  webpack(config, { dev, webpack }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack", "url-loader"],
+    });
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        API_DOMAIN:
+          process.env.NODE_ENV === "production"
+            ? JSON.stringify(process.env.API_URL)
+            : JSON.stringify(process.env.DEV_API_URL),
+      }),
+      new webpack.EnvironmentPlugin(["NODE_ENV"]),
+      new Dotenv({ silent: true }),
+    );
+    return config;
+  },
+};
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,12 @@
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
+import Global from "@styles/globalStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+  return (
+    <>
+      {Global}
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
