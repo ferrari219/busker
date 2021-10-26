34f1629eea643e6580cd58c64fc9199b70671b1f
ferrari219@nate.com <ferrari219@nate.com>
2021-10-26 18:08:23 +0900

    searchresult수정

--- frontend/src/components/04_Templates/Search/Search.tsx
+++ frontend/src/components/04_Templates/Search/Search.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import FootFloat from "src/components/03_Organism/FootFloat";
 import SearchResult from "src/components/03_Organism/SearchResult";
+// /** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
+const searchStyle = css``;
 interface IsearchProps {
   iptClick?: () => void;
 }
 
 const Search = ({ iptClick }: IsearchProps) => {
   return (
-    <div>
+    <div css={searchStyle}>
       <FootFloat iptClick={iptClick} />
-      <SearchResult />
+      <div className="schresult">
+        <SearchResult />
+      </div>
     </div>
   );
 };
